0066c74cf1b44048ea5f71814f86dcfbfe9bebc6
aaguero96 <andre.luiz_1996@hotmail.com>
2024-01-08 18:41:43 -0300

    feat: add env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './infra/database/db.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DbModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/infra/database/db.module.ts
+++ src/infra/database/db.module.ts
@@ -2,21 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-// configService: ConfigService
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async () => {
+      useFactory: async (configService: ConfigService) => {
         return {
           type: 'postgres',
           logger: null,
-          host: 'localhost',
-          port: 5432,
-          username: 'root',
-          password: 'root',
-          database: 'pg-db',
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
           entities: [join(__dirname, './entities/**/*.entity{.js,.ts}')],
           migrations: [join(__dirname, './migrations/**/*{.js,.ts}')],
           autoLoadEntities: true,
